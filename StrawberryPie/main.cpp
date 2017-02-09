b2878edd9f0755e41975179aace1f55531a066e5
Melissa Cat <missmel@4o4.nl>
2017-02-09 21:07:35 +0100

    Disable a bunch of random city stuff

--- StrawberryPie/main.cpp
+++ StrawberryPie/main.cpp
@@ -4,14 +4,28 @@
 #include <Windows.h>
 
 #include <shv/main.h>
+#include <shv/natives.h>
 
 static void appScriptMain()
 {
 	logWrite("Main script running");
 
 	while (true) {
-		WAIT(1000);
-		logWrite("Tick...");
+		VEHICLE::SET_RANDOM_TRAINS(false);
+		VEHICLE::SET_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(0.0f);
+		VEHICLE::SET_PARKED_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(0.0f);
+		VEHICLE::SET_NUMBER_OF_PARKED_VEHICLES(-1);
+		VEHICLE::SET_ALL_LOW_PRIORITY_VEHICLE_GENERATORS_ACTIVE(false);
+		VEHICLE::SET_FAR_DRAW_VEHICLES(false);
+
+		MOBILE::DESTROY_MOBILE_PHONE();
+
+		PED::SET_PED_DENSITY_MULTIPLIER_THIS_FRAME(0.0f);
+		PED::SET_SCENARIO_PED_DENSITY_MULTIPLIER_THIS_FRAME(0.0f, 0.0f);
+
+		UI::DISPLAY_CASH(false);
+
+		WAIT(0);
 	}
 }
 
