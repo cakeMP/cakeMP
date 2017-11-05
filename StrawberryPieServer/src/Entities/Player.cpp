52dd6a9358e139cb5a3bf1e02ccb163241f59665
Codecat <spansjh@gmail.com>
2017-11-05 23:15:22 +0100

    Send nickname instead of player handle in chat message

--- StrawberryPieServer/src/Entities/Player.cpp
+++ StrawberryPieServer/src/Entities/Player.cpp
@@ -136,7 +136,7 @@ void Player::HandleMessage(NetworkMessage* message)
 		logWrite("%s: %s", m_username.c_str(), chatMessage.c_str());
 
 		NetworkMessage* msgChat = new NetworkMessage(NMT_ChatMessage);
-		msgChat->Write(m_handle);
+		msgChat->Write(m_nickname);
 		msgChat->Write(chatMessage);
 		_pServer->m_network.SendMessageToAll(msgChat, m_peer);
 
