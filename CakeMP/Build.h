14456a6d4857b6a4724bdd76c4b8cfa572f0d635
Codecat <spansjh@gmail.com>
2017-11-12 22:18:23 +0100

    Change all mentions of Strawberry to Cake

--- CakeMP/Build.h
+++ CakeMP/Build.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define PROJECT_NAME "Strawberry Pie"
-#define PROJECT_NAME_SHORT "Strawberry"
+#define PROJECT_NAME "CakeMP"
+#define PROJECT_NAME_SHORT "Cake"
 #define PROJECT_VERSION "0.1.000.000"
 #define PROJECT_BUILDTYPE "Development Build"
